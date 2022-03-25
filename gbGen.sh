b803cf90fc48bf4ef2aced8a82401f66b2b3a131
Nagilem <nagilem@hotmail.com>
2022-03-25 16:18:51 -0700

    update to genesis script v1

---
+++ gbGen.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Gunbot Linux Genesis script - This sets up the linux environment to run Gunbot
+
+sudo apt-get update -y -q
+sudo apt-get upgrade -y -q
+sudo apt install wget unzip sqlite -y -q
+cd /home
+sudo wget https://github.com/GuntharDeNiro/BTCT/releases/download/2329/gunthy_linux.zip
+sudo unzip gunthy_linux.zip
+sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.profile 
+nvm install node
+npm install -g npm@latest
+npm install uuid@latest
+npm install --save lodash@latest
+npm install pm2
+
+cd /home/gunthy_linux
+sudo mkdir user_modules
+cd user_modules
+sudo cp -R /home/botmaster/node_modules/lodash .
+cd /home/gunthy_linux
+sudo wget https://github.com/Nagilem/BotRepo/blob/d6eefac907d3715eaa102ee62939dc1cd674aacf/ichi.js
+cd /home
+sudo chmod -R 777 gunthy_linux
