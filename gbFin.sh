640549e061b234438fe8891bbf0ee9c15f40a7e2
Nagilem <nagilem@hotmail.com>
2022-03-25 20:45:06 -0700

    final update

---
+++ gbFin.sh
@@ -0,0 +1,9 @@
+nvm install node
+npm install -g npm@latest
+npm install uuid@latest
+npm install --save lodash@latest
+npm install pm2
+cd /home/gunthy_linux/user_modules
+sudo cp -R /home/botmaster/node_modules/lodash .
+cd /home
+sudo chmod -R 777 gunthy_linux
--- gbGen.sh
+++ gbGen.sh
@@ -7,18 +7,9 @@ sudo apt install wget unzip sqlite -y -q
 cd /home
 sudo wget https://github.com/GuntharDeNiro/BTCT/releases/download/2329/gunthy_linux.zip
 sudo unzip gunthy_linux.zip
-sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.profile 
-nvm install node
-npm install -g npm@latest
-npm install uuid@latest
-npm install --save lodash@latest
-npm install pm2
+sudo rm gunthy_linux.zip
 cd /home/gunthy_linux
 sudo mkdir user_modules
-cd user_modules
-sudo cp -R /home/botmaster/node_modules/lodash .
-cd /home/gunthy_linux
 sudo wget https://raw.githubusercontent.com/Nagilem/BotRepo/main/ichi.js
-cd /home
-sudo chmod -R 777 gunthy_linux
+cd /
+sudo wget 
