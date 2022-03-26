1d2ee84c6489c8000907e386b5fbbe97ae62dbfc
Nagilem <nagilem@hotmail.com>
2022-03-26 06:56:43 -0700

    a23 - continuing fixes for pair purchase

--- ichi.js
+++ ichi.js
@@ -2,7 +2,7 @@
     //custom bot v1 by Rob Esparza
     //Started 3/4/2022, latest update 3/24/2022. See version below.
 
-    const botVer = "4.0.1-a22"
+    const botVer = "4.0.1-a23"
     const _ = gb.method.require(gb.modulesPath + '/lodash')
     
     // constants that need setting to tell bot when to buy / sell
@@ -493,7 +493,7 @@
             console.log(pairOp)
         }
          
-        if (pairOp > 0 && pairEnable = true) {
+        if (pairOp > 0 && pairEnable == true) {
             noOPair = false
             console.log("Pair with assets found!")
         }
